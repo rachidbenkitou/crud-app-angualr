19d2a0183e06453b856102817558c3a67da1b7da
Rachid Benkitou <rachidbenkitou1@gmail.com>
2023-02-25 18:23:55 +0100

    j'ai affecté l'ajout la suppresion et la modification

--- tasks/src/app/components/tasks/tasks.component.ts
+++ tasks/src/app/components/tasks/tasks.component.ts
@@ -10,6 +10,12 @@ import { TaskService } from 'src/app/services/task.service';
 
 export class TasksComponent implements OnInit{
   tasks: Task[]=[];
+  myTask: Task= {
+    label:'',
+    completed:false
+  }
+  editForm:boolean=false;
+  showForm=false;
   constructor(private taskService: TaskService){}
 
   ngOnInit() {
@@ -19,8 +25,58 @@ export class TasksComponent implements OnInit{
   getTasks(){
     this.taskService.findAll()
         .subscribe(tasks=> this.tasks=tasks)
-        
-
   }
+
+  deleteTask(id: number):void{
+    this.taskService.delete(id)
+        .subscribe(()=>{
+            this.tasks=this.tasks.filter(task=> task.id!=id);
+        })   
+  }
+
+  persistTask(){
+    this.taskService.persist(this.myTask)
+        .subscribe((task) => {
+          this.tasks=[task, ...this.tasks];
+          this.resetTask();
+          this.showForm=false;
+        });
+  }
+
+  resetTask(){
+    this.myTask= {
+      label: '',
+      completed: false
+    }
+  }
+
+  toggleCompleted(task:Task){
+    if (!task.id) {
+      console.log("The Is is Undefined.");
+      return;
+    }
+    this.taskService.completed(task.id, task.completed)
+        .subscribe(() =>{
+          task.completed=!task.completed;
+        } )
+  }
+
+  editTask(task:Task){
+    this.myTask= task;
+    this.editForm=true;
+    this.showForm=true;
+  }
+
+  updateTask() {
+    this.taskService.update(this.myTask)
+      .subscribe(task => {
+        this.resetTask();
+        this.editForm = false;
+        this.showForm=false;
+      });
+  }
+  
+
+
 
 }
--- tasks/src/app/services/task.service.ts
+++ tasks/src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Task } from '../models/task';
 
 @Injectable({
@@ -7,9 +8,28 @@ import { Task } from '../models/task';
 })
 export class TaskService {
 
+  apiUrl="http://localhost:5000/tasks";
   constructor(private http: HttpClient) { }
 
   findAll(){
-    return this.http.get<Task[]>("http://localhost:5000/tasks");
+    return this.http.get<Task[]>(this.apiUrl);
   }
+
+  delete(id: number){
+    return this.http.delete(`${this.apiUrl}/${id}`)
+  }
+
+  persist(task:Task){
+    return this.http.post<Task>(this.apiUrl,task);
+  }
+
+// This allows us to change the course completion, if is it completed we make uncompleted and the inverse.
+completed(id:number, completed:boolean){
+  return this.http.patch(`${this.apiUrl}/${id}`,{completed: !completed})
+}
+
+update(task:Task):Observable<Task>{
+  return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+}
+
 }
